96707ac096396e9569c01da69e1e35d98ed51ab0
nikzral <57074003+nikzral@users.noreply.github.com>
2020-12-08 14:34:57 -0800

    n

--- access.js
+++ access.js
@@ -1,7 +1,7 @@
 function requestaccess() {
     var inp = document.getElementById("nrac").value;
     if (inp == "412d003e4977173f4cfe1e9cc6451b75") {
-        window.open("nr.html");
+        window.open("nr.html","_self");
     } else {
         window.alert("INVALID TOKEN")
     }
